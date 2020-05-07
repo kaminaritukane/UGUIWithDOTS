d758574224526024819490d9c9387a6b8ab3993f
GavinZhu <gavin.zhu@pixelmatic.com>
2020-05-07 14:27:38 +0800

    Add Window minimize animation

---
+++ Assets/Windows/Animations/WindowsAnim.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowsAnim : MonoBehaviour
+{
+    public delegate void ShowWindowDelegate();
+    public delegate void HideWindowDelegate();
+
+    public event ShowWindowDelegate ShowWindowEventHandler;
+    public event HideWindowDelegate HideWindowEventHandler;
+
+    [SerializeField] private GameObject window;
+    private Animator animator;
+    private float transitTime = 0.2f;
+
+    void Awake()
+    {
+        animator = window.GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        ShowWindowEventHandler += ShowWindow;
+        HideWindowEventHandler += HideWindow;
+    }
+
+    private void OnDisable()
+    {
+        ShowWindowEventHandler -= ShowWindow;
+        HideWindowEventHandler -= HideWindow;
+    }
+
+    //For Test Animation
+    private bool isShow = true;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isShow = !isShow;
+            animator.SetBool("isShow", isShow);
+        }
+    }
+        
+    public void ShowWindow()
+    {
+        window.SetActive(true);
+        animator.SetBool("isShow", true);
+    }
+
+    public void HideWindow()
+    {
+        animator.SetBool("isShow", false);
+        Invoke("DisableWindow", transitTime);
+    }
+
+    private void DisableWindow()
+    {
+        window.SetActive(false);
+    }
+}
