69e1fc0311c7c5b94820aae1988173b3f66de7f1
GavinZhu <gavin.zhu@pixelmatic.com>
2020-05-07 15:49:20 +0800

    Create scroll lists prefabs.

---
+++ Assets/ScrollList/ScrollBarSteps.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollBarSteps : MonoBehaviour
+{
+    [SerializeField] private RectTransform content;
+    private Scrollbar scrollbar;
+    private int cellCount;
+
+    private void Awake()
+    {
+        cellCount = content.childCount;
+        scrollbar = GetComponent<Scrollbar>();
+        scrollbar.numberOfSteps = cellCount;
+    }
+}
