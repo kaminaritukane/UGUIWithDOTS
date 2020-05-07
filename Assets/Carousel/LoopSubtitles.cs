3209ba5951882604f4ef0c31368af96df18fe111
GavinZhu <gavin.zhu@pixelmatic.com>
2020-05-07 18:44:07 +0800

    Add Carousel prefab

---
+++ Assets/Carousel/LoopSubtitles.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoopSubtitles : MonoBehaviour
+{
+    [SerializeField] private RectTransform background;
+    [SerializeField] private RectTransform text;
+    [SerializeField] private float loopSpeed = 10;
+    private ContentSizeFitter contentSizeFitter;
+    private RectTransform text2;
+    private float textPosY;
+    private bool showText1 = true;
+    private bool showText2 = false;
+    float backgroundWidth;
+    float textWidth;
+
+    public void UpdateSubtitileContent(string data)
+    {
+        text.GetComponent<Text>().text = data;
+        contentSizeFitter.enabled = true;
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        //With ContentSizeFitter component, can not get the correct with of the text
+        contentSizeFitter.enabled = false;
+        textWidth = text.sizeDelta.x;
+
+        if (textWidth > backgroundWidth)
+        {
+            if (text2 != null)
+                Destroy(text2.gameObject);
+            text2 = Instantiate(text.gameObject, background.transform).GetComponent<RectTransform>();
+            text2.anchoredPosition = new Vector2(background.sizeDelta.x, textPosY);
+        }
+
+        StartCoroutine(MoveText1());
+    }
+
+    private void Start()
+    {
+        contentSizeFitter = text.GetComponent<ContentSizeFitter>();
+        backgroundWidth = background.sizeDelta.x;
+        textPosY = text.anchoredPosition.y;
+        Initialize();
+    }
+
+    private IEnumerator MoveText1()
+    {
+        text2.anchoredPosition = new Vector2(backgroundWidth, textPosY);
+
+        while (showText1)
+        {
+            if (text.anchoredPosition.x > backgroundWidth - textWidth)
+            {
+                text.anchoredPosition = new Vector2(text.anchoredPosition.x - loopSpeed * Time.deltaTime, textPosY);
+            }
+            else if(text2.anchoredPosition.x > 0)
+            {
+                text.anchoredPosition = new Vector2(text.anchoredPosition.x - loopSpeed * Time.deltaTime, textPosY);
+                text2.anchoredPosition = new Vector2(text2.anchoredPosition.x - loopSpeed * Time.deltaTime, textPosY);
+            }
+            else if(text2.anchoredPosition.x <= 0)
+            {
+                showText1 = false;
+                showText2 = true;
+                StartCoroutine(MoveText2());
+            }
+            yield return 0;
+        }
+        yield return null;
+    }
+
+    private IEnumerator MoveText2()
+    {
+        text.anchoredPosition = new Vector2(backgroundWidth, textPosY);
+
+        while (showText2)
+        {
+            if (text2.anchoredPosition.x > backgroundWidth - textWidth)
+            {
+                text2.anchoredPosition = new Vector2(text2.anchoredPosition.x - loopSpeed * Time.deltaTime, textPosY);
+            }
+            else if (text.anchoredPosition.x > 0)
+            {
+                text.anchoredPosition = new Vector2(text.anchoredPosition.x - loopSpeed * Time.deltaTime, textPosY);
+                text2.anchoredPosition = new Vector2(text2.anchoredPosition.x - loopSpeed * Time.deltaTime, textPosY);
+            }
+            else if (text.anchoredPosition.x <= 0)
+            {
+                showText1 = true;
+                showText2 = false;
+                StartCoroutine(MoveText1());
+            }
+            yield return 0;
+        }
+        yield return null;
+    }
+}
