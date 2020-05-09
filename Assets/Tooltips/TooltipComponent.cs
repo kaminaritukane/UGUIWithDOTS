ad12ecaa2e380cd59f2ec3276d94c63604b930ed
GavinZhu <gavin.zhu@pixelmatic.com>
2020-05-09 14:39:46 +0800

    Add work of Tooltips

---
+++ Assets/Tooltips/TooltipComponent.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public enum TooltipAnchor
+{
+    Above,
+    Below,
+    Left, //The tooltip will show in the left of the object, so pos x is minus, and text alignment is MiddleRight
+    Right   
+}
+
+public class TooltipComponent : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private string tooltip;
+    [SerializeField] private TooltipAnchor tooltipAnchor;
+    private RectTransform rectTransform;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        TooltipsManager.Instance.ShowTooltips(rectTransform, tooltipAnchor, tooltip);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        TooltipsManager.Instance.HideTooltips(rectTransform);
+    }
+}
---
+++ Assets/Tooltips/TooltipsManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TooltipsManager : Singleton<TooltipsManager>
+{
+    [SerializeField] private GameObject tooltipsLabel; 
+    private RectTransform rectTransform;
+    private RectTransform parent;
+    private Text text;
+
+    private void Start()
+    {
+        rectTransform = tooltipsLabel.GetComponent<RectTransform>();
+        text = tooltipsLabel.GetComponent<Text>();
+    }
+
+    public void ShowTooltips(RectTransform parent, TooltipAnchor tooltipAnchor, string content)
+    {
+        this.parent = parent;
+        Vector2 pos = parent.anchoredPosition;
+        Vector2 size = parent.sizeDelta;
+        Vector2 labelSize = rectTransform.sizeDelta;
+
+        switch(tooltipAnchor)
+        {
+            case TooltipAnchor.Above:
+                text.alignment = TextAnchor.MiddleCenter;
+                rectTransform.anchoredPosition = pos + new Vector2(0, size.y / 2 + labelSize.y / 2); 
+                break;
+            case TooltipAnchor.Below:
+                text.alignment = TextAnchor.MiddleCenter;
+                rectTransform.anchoredPosition = pos - new Vector2(0, size.y / 2 + labelSize.y / 2); 
+                break;
+            case TooltipAnchor.Left:
+                text.alignment = TextAnchor.MiddleRight;
+                rectTransform.anchoredPosition = pos - new Vector2(size.x /2 + labelSize.x /2, 0); 
+                break;
+            case TooltipAnchor.Right:
+                text.alignment = TextAnchor.MiddleLeft;
+                rectTransform.anchoredPosition = pos + new Vector2(size.x / 2 + labelSize.x / 2, 0); 
+                break;
+            default: Debug.LogError("TooltipAnchor was not in the list!"); break;
+        }
+
+        text.text = content;
+        tooltipsLabel.SetActive(true);
+    }
+
+    public void HideTooltips(RectTransform parent)
+    {
+        if(this.parent == parent)
+        {
+            tooltipsLabel.SetActive(false);
+        }
+    }
+}
+
