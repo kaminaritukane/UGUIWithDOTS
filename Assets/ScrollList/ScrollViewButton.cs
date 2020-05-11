653e8eed580e32d95629c3078077f14501d60db6
GavinZhu <gavin.zhu@pixelmatic.com>
2020-05-11 17:33:51 +0800

    Add Scroll Button and Manager。

---
+++ Assets/ScrollList/ScrollViewButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class StringEvent : UnityEvent<string>
+{
+
+}
+
+public class ScrollViewButton : MonoBehaviour, IPointerClickHandler
+{
+    public StringEvent stringEvent = new StringEvent();
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        stringEvent?.Invoke(gameObject.name);
+    }
+}
---
+++ Assets/ScrollList/ScrollViewManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollViewManager : MonoBehaviour
+{
+    [SerializeField] GameObject content;
+
+    void Start()
+    {
+        ScrollViewButton[] scrollViewButtons = content.GetComponentsInChildren<ScrollViewButton>();
+        for (int i = 0; i < scrollViewButtons.Length; i++)
+        {
+            scrollViewButtons[i].stringEvent.AddListener(ButtonHandler);
+        }
+    }
+
+    void ButtonHandler(string buttonName)
+    {
+        //Change model to that one.
+        Debug.Log(buttonName);
+    }
+}
