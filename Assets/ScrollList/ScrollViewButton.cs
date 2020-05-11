01c6684e6c69d175603868a54ba92deac497cca6
GavinZhu <gavin.zhu@pixelmatic.com>
2020-05-11 17:36:38 +0800

    Make select work for button

--- Assets/ScrollList/ScrollViewButton.cs
+++ Assets/ScrollList/ScrollViewButton.cs
@@ -7,11 +7,16 @@ public class StringEvent : UnityEvent<string>
 
 }
 
-public class ScrollViewButton : MonoBehaviour, IPointerClickHandler
+public class ScrollViewButton : MonoBehaviour, IPointerClickHandler, ISelectHandler
 {
     public StringEvent stringEvent = new StringEvent();
 
     public void OnPointerClick(PointerEventData eventData)
+    {
+        //stringEvent?.Invoke(gameObject.name);
+    }
+
+    public void OnSelect(BaseEventData eventData)
     {
         stringEvent?.Invoke(gameObject.name);
     }
